2d29451b6d247140468868a81c482fdc2b93b138
reynaldo0 <reynaldooyusellino@gmail.com>
2024-07-11 13:51:41 +0700

    new : theme change functional

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client"
+
+import React, { useContext } from 'react'
 import styles from './ThemeToggle.module.css'
+import Image from 'next/image'
+import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToggle = () => {
+
+  const {toggle, theme} = useContext(ThemeContext)
+
+  console.log(theme)
+  
   return (
-    <div>ThemeToggle</div>
+    <div className={styles.container} onClick={toggle}>
+      <Image src='/svg/moon.svg' alt='' width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src='/svg/sun.svg' alt='' width={14} height={14} />
+    </div>
   )
 }
 
