058d0debf42e3c575defe27d9a9d34fb1b9dbbae
reynaldo0 <reynaldooyusellino@gmail.com>
2024-07-13 17:29:30 +0700

    new : menu content

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import styles from './Menu.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const Menu = () => {
   return (
-    <div className={styles.container}>Menu</div>
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>Kabar Terpanas?</h2>
+      <h1 className={styles.title}>Paling populer</h1>
+      <div className={styles.items}>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.progamming}`}>Progamming</span>
+            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, autem.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Reys - </span>
+              <span className={styles.date}>2 Januari 2099</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
   )
 }
